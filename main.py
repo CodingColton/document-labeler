b3d96bb6af4ef89a987f4dda353186827798df6b
CodingColton <100506105+CodingColton@users.noreply.github.com>
2022-04-07 18:33:43 -0500

    Revise comments.

--- main.py
+++ main.py
@@ -9,18 +9,20 @@ import fpdf
 import os
 import sys
 
-sticker_file = r'resources/exhibit-label.jpg' #sets label picture location
-# defines where this blank pdf is located to be modified later
+# get file locations
+sticker_file = r'resources/exhibit-label.jpg'
 temporary_text_file = r'resources/temporary-text.pdf'
-# this references where to find the documents that the user needs labeled
 document_folder_path = r'DOCUMENTS-GO-HERE/'
-document_number = 1 # sets the starting number for the first document R-"1"
-get_user_input.get_user_input_start_number() #get input for starting number
 
-party_type = 'p' #create variable for Party at default of 'p' Petitioner
-get_user_input.get_user_input_for_party() # get input for party type
+# set exhibit number default and get user input
+document_number = 1 
+get_user_input.get_user_input_start_number() 
 
-# creates a list with all .pdf files in the directory stored to it
+# set party default and get user input
+party_type = 'p' 
+get_user_input.get_user_input_for_party() 
+
+# create a list with all .pdf files in the directory stored to it
 folder_doc_list = list(glob.glob(document_folder_path + '*.pdf'))
 print(str(folder_doc_list)) # for debugging purposes
 
@@ -30,13 +32,12 @@ with open(r'resources/exhibit-list.txt', 'w') as list_text_file:
 with open(r'resources/success-and-fail-log.txt', 'w') as success_log_text_file:
     success_log_text_file.write('')
 
-#This for loop handles the labeling of each pdf document
+#Label each document in list
 for file in folder_doc_list:
     #this variable will decide if the text log shows a SUCCESS or FAIL next
-    #to the file name. Placed here to reset value to default of True
+    #to the file name. Set here to reset value to default of True for each loop
     file_successfully_labeled = True
 
-    #get values
     targeted_pdf = PdfFileReader(str(file))
     targeted_pdf_page = targeted_pdf.getPage(0)
     target_pdf_width = float(targeted_pdf_page.mediaBox.getWidth()) * 0.352
@@ -49,7 +50,7 @@ for file in folder_doc_list:
 
     font_size = ((sticker_width + sticker_height) * 14 / 480) * 10
 
-    #sets the text properties that will display on the label
+    #set the text properties that will display on the label
     text_pdf_page = fpdf.FPDF('p', 'mm',
             [target_pdf_width, target_pdf_height])
     text_pdf_page.add_page()
@@ -57,13 +58,16 @@ for file in folder_doc_list:
     text_pdf_page.set_xy(5, 5)
     text_pdf_page.image(sticker_file, 0, 0, sticker_width, sticker_height)
 
+    #write text on the label
     if get_user_input.party_type == 'r':
-        text_pdf_page.cell(target_pdf_width / 9, sticker_width * .4,
+        text_pdf_page.cell(target_pdf_width / 9,
+                sticker_width * .4,
                 txt='R-' + str(get_user_input.document_number).zfill(2), ln=0)
 
         text_pdf_page.output(temporary_text_file)
     else:
-        text_pdf_page.cell(target_pdf_width / 9, sticker_width * .4,
+        text_pdf_page.cell(target_pdf_width / 9,
+                sticker_width * .4,
                 txt='P-' + str(get_user_input.document_number).zfill(2), ln=0)
 
         text_pdf_page.output(temporary_text_file)
@@ -73,7 +77,6 @@ for file in folder_doc_list:
     overlay_file = PdfFileReader(temporary_text_file)
 
     target_first_page = targeted_pdf.getPage(0)
-
     target_first_page.mergePage(overlay_file.getPage(0))
 
     pdf_writer = PdfFileWriter()
@@ -88,11 +91,9 @@ for file in folder_doc_list:
     except:
         file_successfully_labeled = False
 
-    #gets just the name of the pdf file(and not the directory location)
-    # to be used when renaming the file
+    #gets only the name of the pdf file(and not the directory location)
     name_of_pdf_no_directory = str(file.split("/", 1)[1])
-    # The code below goes through each file in DOCUMENTS-GO-HERE and adds
-    # P-1 or R-1 (depending on party_type set above) to the start of the file
+    # Rename the file
     if get_user_input.party_type.lower() == 'r':
         #add to exhibit list text file
         with open(r'resources/exhibit-list.txt', 'a') as list_text_file_output:
