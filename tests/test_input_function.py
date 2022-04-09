8b4277a21badc8e5fbfeea8ae0e63808abdc7d1a
CodingColton <100506105+CodingColton@users.noreply.github.com>
2022-04-09 10:27:05 -0500

    Create test file for user inputs - BROKEN.

---
+++ tests/test_input_function.py
@@ -0,0 +1,9 @@
+# from unittest import mock
+# from unittest import TestCase
+# import unittest
+# from get_user_input import get_user_input_start_number, get_user_input_for_party
+
+# class InputTestCase(unittest.TestCase):
+#     "Tests for the user input for the party type and starting document number"
+
+#     def test_sets_correct_number(self):
