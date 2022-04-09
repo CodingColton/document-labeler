ae21d0a2f617a519441e8f21d41cf08c86fbd593
CodingColton <100506105+CodingColton@users.noreply.github.com>
2022-04-09 12:33:33 -0500

    Clean up imports in test file.

--- tests/test_doc_labeling.py
+++ tests/test_doc_labeling.py
@@ -1,9 +1,7 @@
-from unittest import TestCase
 import unittest
 import sys, os
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
-
 from src.label_documents import log_output_success
 
 class DocumentLabelingTestCase(unittest.TestCase):
