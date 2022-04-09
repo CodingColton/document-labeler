11a90df4defd6e1e300650b2e2eb94c7d300d15f
CodingColton <100506105+CodingColton@users.noreply.github.com>
2022-04-09 12:30:41 -0500

    Pass test for logging success of label.

--- src/label_documents.py
+++ src/label_documents.py
@@ -1,10 +1,16 @@
 def log_output_success(file_successfully_labeled, file):
         #print SUCCESS or FAIL in the log text file for each document
         if file_successfully_labeled == True:
+            output_text_for_log = "SUCCESS " + str(file) #helpful for testing
             with open(r'resources/success-and-fail-log.txt',
                     'a') as log_text_file_output:
-                log_text_file_output.write("\nSUCCESS " + str(file))
+                log_text_file_output.write("\n" + output_text_for_log)
+            return output_text_for_log
+                
         else:
+            output_text_for_log = "FAIL " + str(file) #helpful for testing
             with open(r'resources/success-and-fail-log.txt',
                     'a') as log_text_file_output:
-                log_text_file_output.write("\nFAIL " + str(file))
+                log_text_file_output.write("\n" + output_text_for_log)
+
+            return output_text_for_log
--- tests/test_doc_labeling.py
+++ tests/test_doc_labeling.py
@@ -1,5 +1,9 @@
 from unittest import TestCase
 import unittest
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+
 from src.label_documents import log_output_success
 
 class DocumentLabelingTestCase(unittest.TestCase):
@@ -7,7 +11,7 @@ class DocumentLabelingTestCase(unittest.TestCase):
 
     def test_success_fail(self):
         #Test if the log correctly outputes 'SUCCESS' or 'FAIL'
-        success_output = log_output_success(True, 'file_name.pdf')
-        self.assertEqual(success_output, 'SUCCESS file_name.pdf')
+        success_output = log_output_success(False,'file_name.pdf')
+        self.assertEqual(success_output, 'FAIL file_name.pdf')
 
 unittest.main()
